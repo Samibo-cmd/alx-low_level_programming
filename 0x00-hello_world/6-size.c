0480d92e15f6236f90e333ab17166cebea1561f7
Samuel Eibo <samueleibo12@gmail.com>
2023-02-16 18:09:31 -0800

    modify

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,11 +16,11 @@ int main(void)
 
 	/* sizeof evaluates the size of a variables */
 
-printf("Size of char: %zu byte\n", sizeof(charType));
-printf("Size of int: %zu bytes\n", sizeof(intType));
-printf("Size of long int: %zu bytes\n", sizeof(longIntType));
-printf("Size of long long int: %zu bytes\n", sizeof(longLongIntType));
-printf("Size of float: %zu bytes\n", sizeof(floatType));
+printf("Size of char: %zu byte", sizeof(charType));
+printf("Size of int: %zu bytes", sizeof(intType));
+printf("Size of long int: %zu bytes", sizeof(longIntType));
+printf("Size of long long int: %zu bytes", sizeof(longLongIntType));
+printf("Size of float: %zu bytes", sizeof(floatType));
 
 return (0);
 }
