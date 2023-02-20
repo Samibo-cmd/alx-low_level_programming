30d0dddf88f8c7a43acc99606da64b7e2bc61e15
Samuel Eibo <samueleibo12@gmail.com>
2023-02-20 03:22:11 -0800

    Modify

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,17 +10,17 @@ int main(void)
 {
 	char charType;
 	int intType;
-	long int longIntType;
+	long int c;
 	long long int d;
 	float floatType;
 
 	/* sizeof evaluates the size of a variables */
 
-printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(charType));
-printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(intType));
-printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(longIntType));
-printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(floatType));
+printf("Size of char: %lu byte\n", (unsigned long)sizeof(charType));
+printf("Size of int: %lu bytes\n", (unsigned long)sizeof(intType));
+printf("Size of long int: %lu bytes\n", (unsigned long)sizeof(c));
+printf("Size of long long int: %lu bytes\n", (unsigned long)sizeof(d));
+printf("Size of float: %lu bytes\n", (unsigned long)sizeof(floatType));
 return (0);
 }
 
