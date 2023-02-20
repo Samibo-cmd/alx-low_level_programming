6fc4bbb92091c83b830f413888262ae9fb926af0
Samuel Eibo <samueleibo12@gmail.com>
2023-02-20 15:08:12 -0800

    create new file 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print digits in base 10
+ *
+ * Return: return integer value of zero
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		printf("%d", num);
+	}
+	return (0);
+}
