81fb50e653f965faafcedde9a10a7e88a04d0cfd
Samuel Eibo <samueleibo12@gmail.com>
2023-02-20 13:41:00 -0800

    add new file 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print upper and lowercase alphabet
+ *
+ * Return: return 0 value
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'Z'; i++)
+	{
+	printf("%c \n", i);
+	}
+	return (0);
+}
