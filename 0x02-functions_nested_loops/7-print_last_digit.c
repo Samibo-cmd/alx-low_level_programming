7fa8b59dec3d0426e024913b904f45b3bca7d01e
Samuel Eibo <samueleibo12@gmail.com>
2023-02-22 09:17:16 -0800

    create new file 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit of number
+ * @n: passed in the number
+ * Return: an integer value.
+ */
+
+int print_last_digit(int n)
+{
+	int lastDigit;
+
+	lastDigit = n % 10;
+	return (lastDigit);
+}
