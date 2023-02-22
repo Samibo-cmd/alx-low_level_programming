cd0e8b1332fcdfb38c034d9282d282cabe524c77
Samuel Eibo <samueleibo12@gmail.com>
2023-02-22 05:05:56 -0800

    new file 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - computes absolute value of an integer
+ * @n: an integer passed
+ * Return: an integer value
+ */
+
+int _abs(int n)
+{
+	_abs(14);
+
+	_putchar(n);
+	_putchar('\n');
+	return (0);
+}
