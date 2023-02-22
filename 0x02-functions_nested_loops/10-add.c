bdb43f62c15a3e28dab07bb9bb7b223b60c7fb67
Samuel Eibo <samueleibo12@gmail.com>
2023-02-22 11:58:38 -0800

    add new file 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - add two integers
+ * @a: pass an integer
+ * @b: pass an integer
+ * Return: an integer value.
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
