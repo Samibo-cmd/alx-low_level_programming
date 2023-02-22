528b552c9f6ff81ef07c7ddb341a45c9e308b0b6
Samuel Eibo <samueleibo12@gmail.com>
2023-02-22 12:43:06 -0800

    add new file 11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - print natural numbers
+ * @n: pass integer
+ * Return: an integer
+ */
+
+void print_to_98(int n)
+{
+	n = 0;
+	while (n <= 98)
+	{
+		_putchar(n);
+		_putchar(',');
+		_putchar(' ');
+		n++;
+	}
+	_putchar('\n');
+}
