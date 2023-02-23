18f534290dadf95f290f7e0b2a36f04f082f22f4
Samuel Eibo <samueleibo12@gmail.com>
2023-02-23 11:58:17 -0800

    modify

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,13 +8,14 @@
 
 void print_alphabet_x10(void)
 {
+	char alp[26];
 	int i, j;
 
 	for (i = 1; i <= 10; i++)
 	{
 		for (j = 'a'; j <= 'z'; j++)
 		{
-			_putchar(j);
+			_putchar(alp[j]);
 		}
 		_putchar('\n');
 	}
