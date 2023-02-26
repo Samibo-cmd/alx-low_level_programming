419142becefa0b97ea89df83d92ac08d57379384
Samuel Eibo <samueleibo12@gmail.com>
2023-02-26 09:02:01 -0800

    modify

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,13 +12,11 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-	int n;
+	int n, l;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-
-	int l;
 
 	l = n % 10;
 	if (l > 5)
