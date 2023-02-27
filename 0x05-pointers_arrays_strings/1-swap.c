36f104c68f5817672ecf4b76164b1e9718ac3ad3
Samuel Eibo <samueleibo12@gmail.com>
2023-02-27 11:39:35 -0800

    add new file

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - function that swaps the values of variables
+ * @a: pass in first integer
+ * @b: pass in second integer
+ * Return: Always 0.
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
