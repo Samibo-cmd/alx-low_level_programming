b7f3486529b14af8eb5b3e136843005aac909be3
Samuel Eibo <samueleibo12@gmail.com>
2023-02-28 11:41:39 -0800

    create new file

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -2,8 +2,9 @@
 
 /**
  * puts_half - prints half of a string
+ * if odd len, n = (length_of_the_string - 1) / 2
  * @str: pass in a string
- * Return: Always 0.
+ * Return: half of input
  */
 
 void puts_half(char *str)
@@ -12,18 +13,12 @@ void puts_half(char *str)
 
 	i = 0;
 	for (a = 0; str[a] != '\0'; a++)
-	{
 		i++;
-	}
 	n = (i / 2);
 
 	if ((i % 2) == 1)
-	{
 		n = ((n + 1) / 2);
-	}
 	for (a = n; str[a] != '\0'; a++)
-	{
 		_putchar(str[a]);
-	}
 	_putchar('\n');
 }
