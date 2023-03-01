609238fb42d5e5d83296ef4269214c3fb81b49d8
Samuel Eibo <samueleibo12@gmail.com>
2023-02-28 16:27:37 -0800

    new file

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies the string pointed by src
+ * @dest: pass first value
+ * @src: pass in second value
+ * Return: Always 0.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int l = 0;
+	int x = 0;
+
+	while (*(src + 1) != '\0')
+	{
+		l++;
+	}
+	for (; x < l; x++)
+	{
+		dest[x] = src[x];
+	}
+	dest[l] = '\0';
+	return (dest);
+}
