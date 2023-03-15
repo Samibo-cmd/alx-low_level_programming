4fedadc6a892d849182b4a055b5110452b73307d
Samuel Eibo <samueleibo12@gmail.com>
2023-03-15 13:14:56 -0700

    modify

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,17 +13,17 @@ int main(int argc, char *argv[])
 {
 	int result, num1, num2;
 
-	if (argc == 1 || argc > 3)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	else
+	if (argc == 3)
 	{
 		num1 = atoi(argv[1]);
 		num2 = atoi(argv[2]);
 		result = num1 * num2;
 		printf("%d\n", result);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
 	}
 	return (0);
 }
