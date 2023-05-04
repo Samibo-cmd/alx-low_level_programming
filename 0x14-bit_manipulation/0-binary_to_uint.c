1a198f83d1f2065486ac2a2ffe49c50f4367b24d
Samuel Eibo <samueleibo12@gmail.com>
2023-05-04 05:32:15 -0700

    start new project 0x14-bit_manipulation

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts binary number to unsigned int.
+ * @b: strings to be converted
+ * Return: unsigned int
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0;
+	int i;
+
+	if (b == NULL)
+		return (0);
+
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		result = 2 * result + (b[i] - '0');
+	}
+	return (result);
+}
