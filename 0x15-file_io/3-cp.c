8dea4c5512462c00b24a6e506bc41f76df75cda9
Samuel Eibo <samueleibo12@gmail.com>
2023-05-08 13:02:19 -0700

    add new file 3-cp.c

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,99 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+char *create_buffer(char *file);
+void close_file(int fd);
+
+/**
+ * create_buf - gives bytes to buf.
+ * @file: The file that buf stores chars in.
+ * Return: A pointer to buf.
+ */
+
+char *create_buf(char *file)
+{
+	char *buf;
+
+	buf = malloc(sizeof(char) * 1024);
+
+	if (buf == NULL)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't write to %s\n", file);
+		exit(99);
+	}
+
+	return (buf);
+}
+
+/**
+ * close_file - Closes all file descriptors.
+ * @fl: file descriptor to be closed.
+ */
+
+void close_file(int fl)
+{
+	int p;
+
+	p = close(fl);
+
+	if (p == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fl %d\n", fl);
+		exit(100);
+	}
+}
+
+/**
+ * main - Copies contents from one file to another.
+ * @argc: number of arguments given to the program.
+ * @argv: pointers to the arguments.
+ * Return: 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int from, to, r, w;
+	char *buf;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	buf = create_buf(argv[2]);
+	from = open(argv[1], O_RDONLY);
+	r = read(from, buf, 1024);
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	do {
+		if (from == -1 || r == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't read from file %s\n", argv[1]);
+			free(buf);
+			exit(98);
+		}
+
+		w = write(to, buf, r);
+		if (to == -1 || w == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", argv[2]);
+			free(buf);
+			exit(99);
+		}
+
+		r = read(from, buf, 1024);
+		to = open(argv[2], O_WRONLY | O_APPEND);
+
+	} while (r > 0);
+
+	free(buf);
+	close_file(from);
+	close_file(to);
+
+	return (0);
+}
