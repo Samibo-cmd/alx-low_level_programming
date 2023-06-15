34050a1a076b79fee6677c40cb4073148295bc7d
Samuel <samueleibo12@gmail.com>
2023-06-15 15:54:22 -0700

    modify

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -3,6 +3,7 @@
 /**
  * dlistint_len - returns the number of elements in a linked dlistint_t list.
  * @h: the linked list whose elements are counted.
+ * Return: an integer.
  */
 
 size_t dlistint_len(const dlistint_t *h)
